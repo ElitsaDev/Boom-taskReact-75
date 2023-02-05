bba8f2357b559752fcd81db26bcb54bfe7adaa79
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-02-05 20:46:35 +0200

    add localStorage in React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [node, setNode] = useState("");
+
+  useEffect(() => {
+    window.localStorage.setItem("node", node);
+  }, [node]);
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea className="textarea is-large" placeholder="Notes..."
+              value={node}
+              onChange={(e) => {
+                setNode(e.target.value);
+              }}
+            />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button className="button is-large is-primary is-active" 
+                onClick={() => window.localStorage.setItem("node", node)}>Save</button>
+        <button className="button is-large" 
+                onClick={() => window.localStorage.clear()}>Clear</button>
       </div>
     </div>
   );
